942efcb4eb791af4e2dffe8f9333ea51fcaae88a
gabaptistella <gabrielbaptistella18@hotmail.com>
2026-03-29 09:49:20 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
+const express = require("express");
+const app = express();
 
+app.use(express.json());
+
+// rota teste
+app.get("/", (req, res) => {
+  res.send("API Viralizou rodando 🚀");
+});
+
+// IMPORTANTE: usar PORT do Railway
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server rodando na porta " + PORT);
+});
