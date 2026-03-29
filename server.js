cf5acb4024d680eb8e4f2da4cd3f23cb83c49d0a
gabaptistella <gabrielbaptistella18@hotmail.com>
2026-03-29 11:10:39 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const app = express();
 
-// rota simples
 app.get("/", (req, res) => {
-  res.send("Viralizou backend rodando 🚀");
+  res.send("Backend funcionando 🚀");
 });
 
-// 🔥 IMPORTANTE (compatível com Railway e local)
 const PORT = process.env.PORT || 3000;
 
-// escutar na porta correta
 app.listen(PORT, "0.0.0.0", () => {
   console.log("Rodando na porta " + PORT);
 });
